6499f26812096a532490f99073f4dcd50c03f7ec
Andrés Córdova <abc.cordova@gmail.com>
2022-06-06 10:03:02 -0600

    proyecto inicial - superflights api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,6 @@
-import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { PassengerModule } from './passenger/passenger.module';
-import { FlightModule } from './flight/flight.module';
-import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
@@ -14,11 +9,6 @@ import { ConfigModule } from '@nestjs/config';
       envFilePath: ['.env.development'],
       isGlobal: true,
     }),
-    MongooseModule.forRoot(process.env.URI_MONGODB),
-    UserModule,
-    PassengerModule,
-    FlightModule,
-    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
